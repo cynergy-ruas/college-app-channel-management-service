e2d30979171c8edb51e0bbfd614904dd7c236561
ChochChill <chaithanya1359@gmail.com>
2020-12-16 21:42:39 +0530

    added requirements.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,6 @@
 
-MONGODB_URI ="mongodb+srv://user1:user1access@cluster0.fomdd.mongodb.net/springcheck?retryWrites=true&w=majority"
+MONGODB_URI ="mongodb+srv://<username>:<password>@cluster0.fomdd.mongodb.net/springcheck?retryWrites=true&w=majority"
 PORT = 8000
-DB_NAME= "channelCk"
+DB_NAME= "<db_name>"
 CHANNEL_DB= "channels"
 #use channels as your collection name
